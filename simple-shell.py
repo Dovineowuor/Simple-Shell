dfbb8a1ca6843d46fb6f416f18ccc6d8da863727
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-02-14 17:01:34 +0300

    Update simple-shell.py

--- simple-shell.py
+++ simple-shell.py
@@ -10,6 +10,45 @@ while True:
 
     # Parse command and arguments
     command, *args = user_input.split()
+import readline
+
+# Set up history file and enable command history
+history_file = ".my_shell_history"
+readline.read_history_file(history_file)
+readline.set_history_length(1000)
+
+# Define function for retrieving previous command
+def get_previous_command():
+    current_history_len = readline.get_current_history_length()
+    if current_history_len > 1:
+        previous_command = readline.get_history_item(current_history_len - 2)
+        return previous_command.strip()
+    else:
+        return ""
+
+# Main loop
+while True:
+    # Get user input
+    user_input = input("my_shell> ")
+
+    # Handle arrow up to get previous command
+    if user_input == "\033[A":
+        previous_command = get_previous_command()
+        print(previous_command)
+        user_input = previous_command
+
+    # Exit on Ctrl-D
+    if not user_input:
+        break
+
+    # Do something with user input
+    print("You entered:", user_input)
+
+    # Add input to history
+    readline.add_history(user_input)
+
+# Save history file
+readline.write_history_file(history_file)
 
     # Execute command
     if command == "cd":
